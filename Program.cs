bc53eddb83c4700f54b8831d96e67a10aff00ab2
536Cats <88250944+ninetyninecats@users.noreply.github.com>
2026-07-04 14:30:40 -0700

    Set up environment and began implementing reading from memory

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Emulator {
+    static ushort PC;
+    static byte A;
+    static byte X;
+    static byte Y;
+    static byte[] RAM = new byte[0x800];
+    static byte[] ROM = new byte[0x8000];
+    public static void Main(string[] args) {}
+
+    public static byte ReadFromMemory(ushort address) {
+        if (address < 0x800)
+            return RAM[address];
+        else if (address >= 0x8000)
+            return ROM[address - 0x8000];
+        else
+            throw new Exception(
+                $"Memory location {address} not yet implemented");
+    }
+}
